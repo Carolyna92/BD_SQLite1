21594d08ca8b9c32d2ee8797b6bd3990b57304dc
Karolina Muñoz <karo.yoek92@gmail.com>
2017-10-16 22:11:20 -0500

    karolina

---
+++ BD_SQLite1/BD_SQLite1/MP.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BD_SQLite1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MP : ContentPage
+    {
+        public MP()
+        {
+            InitializeComponent();
+        }
+    }
+}
